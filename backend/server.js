1e1eddaf282d7f457ef1a16f44106c95dbd8d45e
slayingMoon <g.kolev15@gmail.com>
2023-03-30 21:55:43 +0200

    set up a connection with the DB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 //dependency imports
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts.js');
 
 //creates express app
@@ -17,7 +18,25 @@ app.use((req, res, next) => {
 //routes to handle requests/responses
 app.use('/api/workouts', workoutRoutes);
 
+//connect to DB
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+    }catch(err) {
+        throw err;
+    }
+};
+
+//Watches for DB connection changes
+mongoose.connection.on('disconnected', () => {
+    console.log('MongoDB disconnected');
+});
+mongoose.connection.on('connected', () => {
+    console.log('MongoDB connected');
+});
+
 //listen for requests
 app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT);
+    connect();
+    console.log('Server listening on port', process.env.PORT);
 });
