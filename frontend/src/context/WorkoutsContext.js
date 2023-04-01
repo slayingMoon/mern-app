ef98f098afe356517ee0e0949824a0d7e712711a
slayingMoon <g.kolev15@gmail.com>
2023-04-01 20:05:59 +0200

    implemented delete workout functionality

--- frontend/src/context/WorkoutsContext.js
+++ frontend/src/context/WorkoutsContext.js
@@ -12,6 +12,10 @@ const workoutsReducer = (state, action) => {
             return {
                 workouts: [action.payload, ...state.workouts]
             }
+        case 'DELETE_WORKOUT':
+            return {
+                workouts: state.workouts.filter((w) => w._id !== action.payload._id)
+            }
         default:
             return state;
     }
