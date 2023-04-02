ec71b9e90dd30fdb99b4b9a38358e77d47df04a4
slayingMoon <g.kolev15@gmail.com>
2023-04-02 11:39:48 +0200

    added login and sign up pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 //pages & components
 import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />}/>
+            <Route path="/signup" element={<Signup />}/>
           </Routes>
         </div>
       </BrowserRouter>
