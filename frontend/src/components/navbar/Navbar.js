14b429230a185b9846d649c156f2edd3089769be
slayingMoon <g.kolev15@gmail.com>
2023-04-02 15:15:36 +0200

    added functionality to register users and to update navbar accordingly

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -3,6 +3,8 @@ import './Navbar.css'
 
 const Navbar = () => {
 
+  const user = JSON.parse(localStorage.getItem('user'));
+
   return (
     <header>
       <div className="container">
@@ -10,10 +12,12 @@ const Navbar = () => {
           <h1>Workout Buddy</h1>
         </Link>
         <nav>
+          {user ? <div>Welcome, {user.email}</div> : 
           <div>
             <Link to="/login">Login</Link>
             <Link to="/signup">Sign up</Link>
           </div>
+          }
         </nav>
       </div>
     </header>
