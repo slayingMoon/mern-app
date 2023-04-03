d41fa80581121458eaec45a6769f631c20885926
slayingMoon <g.kolev15@gmail.com>
2023-04-03 06:35:21 +0200

    added user logout functionality

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,9 +1,15 @@
 import { Link } from 'react-router-dom';
-import './Navbar.css'
+import './Navbar.css';
+import { useLogout } from '../../hooks/useLogout';
 
 const Navbar = () => {
+  const { logout } = useLogout();
 
   const user = JSON.parse(localStorage.getItem('user'));
+
+  const handleClick = () => {
+    logout();
+  };
 
   return (
     <header>
@@ -12,7 +18,12 @@ const Navbar = () => {
           <h1>Workout Buddy</h1>
         </Link>
         <nav>
-          {user ? <div>Welcome, {user.email}</div> : 
+          {user 
+          ? <div>
+              <span>Welcome, {user.email}</span>
+              <button className="logout-btn" onClick={handleClick}>Logout</button>
+            </div> 
+          : 
           <div>
             <Link to="/login">Login</Link>
             <Link to="/signup">Sign up</Link>
