0fb2034eff1dcbd9378edd8d88cc4a010473d446
slayingMoon <g.kolev15@gmail.com>
2023-04-04 06:37:24 +0200

    assigning workouts to users

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -4,8 +4,9 @@ const getEmptyFields = require('../utils/getEmptyFields.js');
 
 // get all workouts
 const getWorkouts = async (req, res) => {
+    const user_id = req.user._id;
     try {
-        const workouts = await Workout.find({}).sort({createdAt: -1});
+        const workouts = await Workout.find({ user_id }).sort({createdAt: -1});
         res.status(200).json(workouts);
     }catch(err) {
         res.status(400).json({error: err.message});
@@ -43,7 +44,8 @@ const createWorkout = async (req, res) => {
 
     //add document to db
     try {
-        const workout = await Workout.create({title, load, reps});
+        const user_id = req.user._id;
+        const workout = await Workout.create({title, load, reps, user_id});
         res.status(200).json(workout);
     }catch(err) {
         res.status(400).json({error: err.message});
--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,7 +1,9 @@
 import { useAuthContext } from "./useAuthContext";
+import { useWorkoutsContext } from './useWorkoutsContext';
 
 export const useLogout = () => {
     const { dispatch } = useAuthContext();
+    const { dispatch: workoutsDispatch } = useWorkoutsContext(); //calling dispatch by using its newly assigned name
 
     const logout = () => {
         // remove user from storage
@@ -9,6 +11,8 @@ export const useLogout = () => {
 
         // dispatch logout action
         dispatch({type: 'LOGOUT'});
+        //clearing the workouts context on user logout
+        workoutsDispatch({type: 'SET_WORKOUTS', payload: null});
     };
 
     return { logout };
